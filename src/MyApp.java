809105950a2bd175c1ec54486c734ecb04ca2892
kninich55 <tahicham5@gmail.com>
2023-03-07 17:09:39 -0500

    addition operation

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class MyApp {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = scan.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = scan.nextInt();
+        Calc calc=new Calc(num1,num2);
+        System.out.println("Addition result: " + calc.add());
+    }
 }
